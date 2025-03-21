ab4a4692290fb6dbff544188b7964fb9713e09cd
SusgUY446 <susguy446@gmail.com>
2025-03-21 12:40:28 +0100

    add examples makefile and improve library

--- examples/window01.c
+++ examples/window01.c
@@ -1,4 +1,5 @@
 #include <btk/btk.h>
+#include <btk/btk_log.h>
 
 
 
@@ -8,6 +9,7 @@ int main() {
     BTKWindow* window = btk_window_create(app, "Hello World", 800, 600, BTK_WINDOW_FLAGS_DEFAULT);
 
     BTKWindow* window2 = btk_window_create(app, "Hello World 2", 800, 600, BTK_WINDOW_FLAGS_DEFAULT);
-    
+
+
     btk_application_run(app);
 }
